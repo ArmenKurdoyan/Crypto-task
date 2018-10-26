c8f3744219acfc1bb3e002d14269c7a3f20c0088
Armen Kurdoyan <knan2599@gmail.com>
2018-10-26 18:53:34 +0400

    easy way

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import Header from './components/Header';
-import Table from './components/Table';
+import Header from './components/Header/Header';
+import Table from './components/Table/Table';
 import './App.css';
 
 class App extends Component {
@@ -8,67 +8,26 @@ class App extends Component {
     super(props);
 
     this.state = {
-      selectedList: `Top 100 Cryptocurrencies by Market Capitalization`,
-      valume: `USD`,
-      sortName: `name`,
-      currentPage: 3,
-      maxPages: 5,
-      showAll: false,
+      sortName: `id`,
+      nextPage: true,
+      currentPage: 1,
       companies: undefined,
     }
-
-    this.handleChangeList = this.handleChangeList.bind(this);
-    this.handleChangeValume = this.handleChangeValume.bind(this);
-    this.handleChangePageDown = this.handleChangePageDown.bind(this);
-    this.handleChangePageUp = this.handleChangePageUp.bind(this);
-    this.handleShowAll = this.handleShowAll.bind(this);
-  }
-
-  handleChangeList(event) {
-    this.setState({
-      selectedList: event.target.value,
-    })
-  }
-
-  handleChangeValume(event) {
-    this.setState({
-      valume: event.target.value,
-    })
-  }
-
-  handleChangePageUp() {
-    this.setState({
-      currentPage: ++this.state.currentPage,
-      showAll: false,
-    })
-  }
-
-  handleChangePageDown() {
-    this.setState({
-      currentPage: --this.state.currentPage,
-      showAll: false,
-    })
-  }
-
-  handleShowAll() {
-    this.setState({
-      showAll: true,
-    })
-    alert(`done`);
   }
 
   render() {
     const data = {
       ...this.state,
-      handleChangeList: this.handleChangeList,
-      handleChangeValume: this.handleChangeValume,
-      handleChangePageUp: this.handleChangePageUp,
-      handleChangePageDown: this.handleChangePageDown,
-      handleShowAll: this.handleShowAll,
+      // handleChangeList: this.handleChangeList,
+      // handleChangeValume: this.handleChangeValume,
+      // handleChangePageUp: this.handleChangePageUp,
+      // handleChangePageDown: this.handleChangePageDown,
+      // handleShowAll: this.handleShowAll,
     }
 
     return (
       <div className="App">
+        <h1>All CryptoCurrencies</h1>
         <Header data={data}/>
         <Table data={data}/>
       </div>
